c29903321de1318ae8bed96355c8150197b3816f
kenokabe <kenokabe@gmail.com>
2014-09-12 06:11:42 +0000

    commit

---
+++ custom.zsh
@@ -0,0 +1,18 @@
+uname -r
+
+ZSH_THEME="ys"
+
+export JAVA_HOME=/usr/lib/jvm/java-7-oracle/jre/bin/java
+
+alias s='git add -A -v; git commit -m "commit"'
+alias sp="s; git push origin master"
+
+alias f="ranger"
+
+alias z="node app"
+alias x="node test"
+
+alias kn="killall -9 node"
+
+alias t="tmux"
+alias h="htop"
