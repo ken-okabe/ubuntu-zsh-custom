245e23790edef3b14fbf3da7df86c6ba9176986a
Ken OKABE <kenokabe@gmail.com>
2014-10-15 00:58:06 +0900

    Update custom.zsh

--- custom.zsh
+++ custom.zsh
@@ -1,18 +1,19 @@
 uname -r
-
 ZSH_THEME="ys"
-
 export JAVA_HOME=/usr/lib/jvm/java-7-oracle/jre/bin/java
-
 alias s='git add -A -v; git commit -m "commit"'
 alias sp="s; git push origin master"
-
+alias a="atom"
 alias f="ranger"
-
 alias z="node app"
 alias x="node test"
-
 alias kn="killall -9 node"
-
 alias t="tmux"
 alias h="htop"
+
+alias gd="/home/ken/Google\ Drive"
+
+alias as="/home/ken/Google\ Drive/atom-shell/atom"
+ 
+source ~/.nvm/nvm.sh
+nvm use v0.11.14
