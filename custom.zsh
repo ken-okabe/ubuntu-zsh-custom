8502dc3187849a79ffd17d09073c16abbdac13b0
Ken OKABE <kenokabe@gmail.com>
2015-04-16 16:05:31 +0900

    Update custom.zsh

--- custom.zsh
+++ custom.zsh
@@ -1,24 +1,22 @@
-uname -r
 ZSH_THEME="ys"
 export JAVA_HOME=/usr/lib/jvm/java-7-oracle/jre/bin/java
+
+uname -r
+sudo modprobe -r mwifiex_usb
 
 alias s='git add -A -v; git commit -m "commit"'
 alias sp="s; git push origin master"
 alias a="atom"
 alias f="ranger"
-alias z="/home/ken/iojs-v1.0.3-linux-x64/bin/iojs app"
-alias x="/home/ken/iojs-v1.0.3-linux-x64/bin/iojs test"
+alias node='/home/ken/node-v0.12.2-linux-x64/bin/node'
+alias npm='/home/ken/node-v0.12.2-linux-x64/bin/npm'
+alias z="node app"
+alias x="node test"
 alias kn="killall -9 node"
 alias t="tmux"
 alias h="htop"
 #alias vmtool=`sudo vmware-config-tools.pl`
+alias gd='/home/ken/Google\ Drive'
 
-alias sc='sudo iwlist mlan0 s'
-alias i='ifconfig & iwconfig'
-alias c='sudo wpa_supplicant -i mlan0 -c wpa.conf -D wext'
-alias ci='sudo dhclient mlan0'
 
 alias wd='sudo modprobe -r mwifiex_usb'
-alias wu='sudo modprobe mwifiex_usb'
-
-alias up='sudo ifconfig mlan0 up'
