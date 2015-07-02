209f7e08b2d1697e2e2d8352cb41ceab439bc842
Ken OKABE <kenokabe@gmail.com>
2015-07-02 15:45:49 +0900

    Update custom.zsh

--- custom.zsh
+++ custom.zsh
@@ -1,19 +1,17 @@
 ZSH_THEME="ys"
-export JAVA_HOME=/usr/lib/jvm/java-7-oracle/jre/bin/java
 
 uname -r
-sudo modprobe -r mwifiex_usb
+
+. ~/.nvm/nvm.sh
+nvm use iojs
 
 alias s='git add -A -v; git commit -m "commit"'
 alias sp="s; git push origin master"
-alias a="atom"
+alias a="atom ./"
 alias f="ranger"
-alias node='/home/ken/node-v0.12.2-linux-x64/bin/node'
-alias npm='/home/ken/node-v0.12.2-linux-x64/bin/npm'
 alias z="node app"
 alias x="node test"
 alias kn="killall -9 node"
-alias t="tmux"
 alias h="htop"
 #alias vmtool=`sudo vmware-config-tools.pl`
 alias gd='/home/ken/Google\ Drive'
