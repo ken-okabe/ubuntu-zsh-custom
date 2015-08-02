f911d8d94c2ee8aadf842b58e8c7302a50b59986
Ken OKABE <kenokabe@gmail.com>
2015-08-02 15:20:02 +0900

    Update custom.zsh

--- custom.zsh
+++ custom.zsh
@@ -9,16 +9,14 @@ alias s='git add -A -v; git commit -m "commit"'
 alias sp="s; git push origin master"
 alias a="atom ./"
 alias f="ranger"
-alias z="node app"
+alias n="babel-node"
 alias x="node test"
-alias kn="killall -9 node"
+alias kn="killall -9 iojs"
 alias h="htop"
 #alias vmtool=`sudo vmware-config-tools.pl`
 alias gd='/home/ken/Google\ Drive'
 
-alias azure='ssh -i ~/.ssh/sshkey/pro.key ken@???????.cloudapp.net -t /bin/zsh'
+alias azure='ssh -i ~/.ssh/sshkey/pro.key ken@??????.cloudapp.net -t /bin/zsh'
 
 
 alias wd='sudo modprobe -r mwifiex_usb'
-
-alias jc='jsx --harmony -w -x jsx jsx/ js/'
