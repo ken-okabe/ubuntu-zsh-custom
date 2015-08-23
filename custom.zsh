848a38318c02d4da6b58e516a6786d10085c2cf5
Ken OKABE <kenokabe@gmail.com>
2015-08-23 16:42:29 +0900

    Update custom.zsh

--- custom.zsh
+++ custom.zsh
@@ -1,25 +1,22 @@
 ZSH_THEME="ys"
-
 uname -r
-
 . ~/.nvm/nvm.sh
 nvm use iojs
-
 alias s='git add -A -v; git commit -m "commit"'
 alias sp="s; git push origin master"
 alias a="atom ./"
-alias f="ranger"
+
 alias n="babel-node"
 alias x="npm test"
 alias kn="killall -9 iojs; killall -9 node"
 alias h="htop"
+alias f="ranger"
+
 #alias vmtool=`sudo vmware-config-tools.pl`
 alias gd='/home/ken/Google\ Drive'
 
 #alias wd='sudo modprobe -r mwifiex_usb'
+alias k='bluetoothctl <<< "connect 00:1F:20:3E:C6:2C"'
+alias m='bluetoothctl <<< "connect DC:2C:26:EA:D0:A9"'
 
-alias b='bluetoothctl <<< "connect 00:1F:20:3E:C6:2C"'
-alias azure='ssh -i ~/.ssh/sshkey/pro.key ken@?????????.cloudapp.net -t /bin/zsh'
-
-
- 
+alias azure='ssh -i ~/.ssh/sshkey/pro.key ken@tt??????.cloudapp.net -t /bin/zsh'
