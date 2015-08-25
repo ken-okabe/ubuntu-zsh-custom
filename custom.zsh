2640b3184fcaa58aca44d43acef09a0a0ddc5f31
Ken OKABE <kenokabe@gmail.com>
2015-08-26 08:06:42 +0900

    Update custom.zsh

--- custom.zsh
+++ custom.zsh
@@ -12,6 +12,7 @@ alias kn="killall -9 iojs; killall -9 node"
 alias h="htop"
 alias f="ranger"
 
+
 #alias vmtool=`sudo vmware-config-tools.pl`
 alias gd='/home/ken/Google\ Drive'
 
@@ -19,4 +20,8 @@ alias gd='/home/ken/Google\ Drive'
 alias k='bluetoothctl <<< "connect 00:1F:20:3E:C6:2C"'
 alias m='bluetoothctl <<< "connect DC:2C:26:EA:D0:A9"'
 
-alias azure='ssh -i ~/.ssh/sshkey/pro.key ken@tt??????.cloudapp.net -t /bin/zsh'
+alias azure='ssh -i ~/.ssh/sshkey/pro.key ken@?????.cloudapp.net -t /bin/zsh'
+
+alias db="babel ./www-db/index.jsx -o ./www-db/index0.js;webpack ./www-db/index0.js ./www-db/index.js;babel-node db"
+
+alias xx="babel ./www-site/index.jsx -o ./www-site/index0.js;webpack ./www-site/index0.js ./www-site/index.js;babel-node site"
