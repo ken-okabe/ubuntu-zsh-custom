65a8fa0dae9533839f3d4ece5af41ba73eb1f471
Ken OKABE <kenokabe@gmail.com>
2015-09-01 08:32:53 +0900

    Update custom.zsh

--- custom.zsh
+++ custom.zsh
@@ -20,7 +20,11 @@ alias gd='/home/ken/Google\ Drive'
 alias k='bluetoothctl <<< "connect 00:1F:20:3E:C6:2C"'
 alias m='bluetoothctl <<< "connect DC:2C:26:EA:D0:A9"'
 
-alias azure='ssh -i ~/.ssh/sshkey/pro.key ken@?????.cloudapp.net -t /bin/zsh'
+alias azure='ssh -i ~/.ssh/sshkey/pro.key ken@??????.cloudapp.net -t /bin/zsh'
+
+#alias db="babel ./www-db/index.jsx -o ./www-db/index0.js;webpack --optimize-minimize ./www-db/index0.js ./www-db/index.js;babel-node db"
+
+#alias xx="babel ./www-site/index.jsx -o ./www-site/index0.js;webpack --optimize-minimize ./www-site/index0.js ./www-site/index.js;babel-node site"
 
 alias db="babel ./www-db/index.jsx -o ./www-db/index0.js;webpack ./www-db/index0.js ./www-db/index.js;babel-node db"
 
